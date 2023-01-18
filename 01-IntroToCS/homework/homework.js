bdf8303e92e13c655f84f83af2a82427f58f4a93
Prep-course <116912265+wesleycr777@users.noreply.github.com>
2023-01-17 22:33:50 -0300

    modificacion-01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var decimal = 0;
+   for (let i = 0; i < num.length; i++) {
+     var valorAgregar = num[i] * 2 ** (num.length - 1 - i);
+     decimal = decimal + valorAgregar;
+   }
+ 
+   return decimal;
 
-function DecimalABinario(num) {}
+}
+
+function DecimalABinario(num) {
+   var binario = [];
+   while (num >= 1) {
+     binario.unshift(num % 2);
+     num = Math.floor(num / 2);
+
+}
+}
+console.log(typeof DecimalABinario(12)); // "1100"
 
 module.exports = {
    BinarioADecimal,
